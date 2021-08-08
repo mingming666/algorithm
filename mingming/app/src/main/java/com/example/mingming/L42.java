45fdd22abb47bcbbe731ee627a537af737fa4be9
mingming666 <zjy1937802858@163.com>
2021-08-08 18:42:25 +0800

    week1 homework

---
+++ mingming/app/src/main/java/com/example/mingming/L42.java
@@ -0,0 +1,25 @@
+package com.example.mingming;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class L42 {
+    public int trap(int[] height) {
+        Deque<Integer> stack = new LinkedList<>();
+        int ans = 0;
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                int top = height[stack.pop()];
+                if (stack.isEmpty()) {
+                    break;
+                }
+                int w = i - stack.peek() - 1;
+                int h = Math.min(height[stack.peek()],height[i]) - top;
+                ans += w * h;
+            }
+            stack.push(i);
+        }
+        return  ans;
+    }
+
+}
---
+++ mingming/app/src/main/java/com/example/mingming/L84.java
@@ -0,0 +1,24 @@
+package com.example.mingming;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class L84 {
+    public int largestRectangleArea(int[] heights) {
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(-1);
+        int maxArea = 0;
+        for (int i = 0; i < heights.length; i++) {
+            while (stack.peek() != -1 && heights[i] > heights[stack.peek()]) {
+                maxArea = Math.max(maxArea, heights[stack.pop()] * (i - stack.peek() - 1));
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            maxArea = Math.max(maxArea, heights[stack.pop()] * (heights.length - 1 - stack.peek()));
+        }
+
+        return maxArea;
+
+    }
+}
